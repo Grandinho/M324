d316a1a8f0e768e52d08950ed4cb61f6f86a97f2
Grand <monyx5861@gmail.com>
2024-11-28 10:51:34 +0100

    made prettier with prettier

--- task-1-npm/my-project/index.js
+++ task-1-npm/my-project/index.js
@@ -1,14 +1,11 @@
-import chalk from "chalk";
-import { generate } from "random-words";
+import chalk from 'chalk'
+import { generate } from 'random-words'
 
 // index.js
-   console.log("Hello, World!");
+console.log('Hello, World!')
 
 // Fügen Sie hier die korrekten Imports ein. Verwenden Sie import, nicht require.
 
-const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 });
-const word2 = generate();
-console.log(
-  `The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`
-);
-
+const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })
+const word2 = generate()
+console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`)
